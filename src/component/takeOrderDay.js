675cccce497f535e9fcb2b6279d2ae4e2947bd94
Naty <natalypc27@gmail.com>
2018-10-12 11:45:07 -0500

    pedidos

--- src/component/takeOrderDay.js
+++ src/component/takeOrderDay.js
@@ -14,9 +14,6 @@ class TakeOrderDay extends Component{
                                 <p>
                                     Cliente:{this.props.name}
                                 </p>
-                                <p>
-                                    Pedidos:
-                                </p>
                             </div>
                             <div className="col-3">
                                 <button>
@@ -27,7 +24,31 @@ class TakeOrderDay extends Component{
                     </div>
                 </section>
                 <div className="section4">
+                    <div className="container">
+                        <div className="row text-center">
+                            <div className="col-12">
+                                <p>
+                                      Tomar Pedido
+                                    </p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-6 orders">
+                                <nav>
+                                    <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                                        <a className="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
+                                        <a className="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>                                    </div>
+                                </nav>
+                                <div className="tab-content" id="nav-tabContent">
+                                    <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
+                                    <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+                                </div>
+                            </div>
+                            <div className="col-6">
 
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
---
+++ src/data/data.js
@@ -0,0 +1,68 @@
+const data=[
+    {
+      "Breakfast": [
+        {
+          "name": "Café americano",
+          "price": 5
+        },
+        {
+          "name": "Café con leche",
+          "price": 7
+        },
+        {
+          "name": "Sandwich de jamón y queso",
+          "price": 5
+        },
+        {
+          "name": "Jugo natural",
+          "price": 5
+        }
+      ],
+      "Lunch/dinner": {
+        "hamburguesas": [
+          {
+            "name": "Hamburguesa",
+            "type": "Simple",
+            "price": 10
+          },
+          {
+            "name": "Hamburguesa",
+            "type": "Doble",
+            "price": 15
+          }
+        ],
+        "acompañamientos": [
+          {
+            "name": "Papas fritas",
+            "price": 5
+          },
+          {
+            "name": "Aros de cebolla",
+            "price": 5
+          }
+        ],
+        "bebidas": [
+          {
+            "name":"Agua",
+            "length": "500ml",
+            "price": 5
+          },
+          {
+            "name":"Agua",
+            "length": "750ml",
+            "price": 8
+          },
+          {
+            "name":"Gaseosa",
+            "length": "500ml",
+            "price": 8
+          },
+          {
+            "name":"Gaseosa",
+            "length": "750ml",
+            "price": 10
+          }
+        ]
+      }
+    }
+  ]

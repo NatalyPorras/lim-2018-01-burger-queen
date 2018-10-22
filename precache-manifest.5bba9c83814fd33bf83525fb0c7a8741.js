7b0290be71b7de56a423e2cdc41618ca495569d8
Naty <natalypc27@gmail.com>
2018-10-22 10:51:44 -0500

    Updates

--- precache-manifest.5bba9c83814fd33bf83525fb0c7a8741.js
+++ precache-manifest.5bba9c83814fd33bf83525fb0c7a8741.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/lim-2018-01-burger-queen/static/js/runtime~main.0dab808f.js"
   },
   {
-    "revision": "3e6dd50fb6b5b794677e",
-    "url": "/lim-2018-01-burger-queen/static/js/main.3e6dd50f.chunk.js"
+    "revision": "ac3ad6b15afb2b6756ed",
+    "url": "/lim-2018-01-burger-queen/static/js/main.ac3ad6b1.chunk.js"
   },
   {
     "revision": "4fd91c74e51506835aa9",
     "url": "/lim-2018-01-burger-queen/static/js/1.4fd91c74.chunk.js"
   },
   {
-    "revision": "3e6dd50fb6b5b794677e",
-    "url": "/lim-2018-01-burger-queen/static/css/main.c2db6795.chunk.css"
+    "revision": "ac3ad6b15afb2b6756ed",
+    "url": "/lim-2018-01-burger-queen/static/css/main.bb06ac70.chunk.css"
   },
   {
-    "revision": "ddb02dc41e36e8ce24590495dcb5ff72",
+    "revision": "06d784514c7b4b39bb70b43a7e24e9b4",
     "url": "/lim-2018-01-burger-queen/index.html"
   }
 ];

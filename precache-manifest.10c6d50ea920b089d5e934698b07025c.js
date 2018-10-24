d66a1233174bfea29699cdfc3014c6b00fcc680b
Nataly Porras <natalypc27@gmail.com>
2018-10-24 16:07:52 -0500

    Updates

---
+++ precache-manifest.10c6d50ea920b089d5e934698b07025c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dab808f1c361c40aacb",
+    "url": "/lim-2018-01-burger-queen/static/js/runtime~main.0dab808f.js"
+  },
+  {
+    "revision": "7bce5c60368e7a4a1c81",
+    "url": "/lim-2018-01-burger-queen/static/js/main.7bce5c60.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/lim-2018-01-burger-queen/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "7bce5c60368e7a4a1c81",
+    "url": "/lim-2018-01-burger-queen/static/css/main.60b0a447.chunk.css"
+  },
+  {
+    "revision": "4b4cc3c28e5e00c2dbc394c119775ca9",
+    "url": "/lim-2018-01-burger-queen/index.html"
+  }
+];
